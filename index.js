84fefb0adc353e301bf79da3ecd49d20d438da2a
Deexith Madas <deexithmadas772@gmail.com>
2023-12-15 18:53:58 +0530

    initialize project

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+    }
+);
+
