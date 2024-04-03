e55f74ed421690f351aa35cc171c10e1378505ef
Kunal Agrawal <66416646+kunalagra@users.noreply.github.com>
2024-04-04 03:33:26 +0530

    Update bank.js

--- routes/bank.js
+++ routes/bank.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Bank = require('../models/Bank');
 const authenticateToken = require('../middleware/authenticateToken');
-
+const UserInfo = require('../models/UserInfo');
 
 
 router.get('/', authenticateToken, async (req, res) => {
