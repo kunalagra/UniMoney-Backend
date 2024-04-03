f3e2b45f3604481f5adbd025269dce6ac83d0ad0
Kunal Agrawal <66416646+kunalagra@users.noreply.github.com>
2024-04-04 01:02:26 +0530

    Update Bank.js

--- models/Bank.js
+++ models/Bank.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const bankSchema = new mongoose.Schema({
     name: String,
     img: String,
+    code: {type: String, unique: true },
+    website: String
 })
 
 module.exports = mongoose.model('Bank', bankSchema);
