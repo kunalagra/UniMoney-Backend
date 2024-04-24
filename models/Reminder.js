8015c47baeb008ae355ad5b07ad5602e5018d4f6
Deexith Madas <deexithmadas772@gmail.com>
2024-04-24 22:13:53 +0530

    updeted reminder

--- models/Reminder.js
+++ models/Reminder.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const reminderSchema = new mongoose.Schema({
     title: String,
     date: Date,
+    description: String,
+    amount: Number,
+    category: {type: Schema.Types.ObjectId, ref: 'Category'},
+    repeat: string,
 })
 
 module.exports = mongoose.model('Reminder', reminderSchema);
--- routes/reminder.js
+++ routes/reminder.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const Bank = require('../models/Reminder');
 const authenticateToken = require('../middleware/authenticateToken');
 const UserInfo = require('../models/UserInfo');
+const Reminder = require('../models/Reminder');
+const Category = require('../models/Category');
 
 
 router.get('/', authenticateToken, async (req, res) => {
     try {
         const userInfo = await UserInfo.findById({ _id: req.user._id })
-        res.json(userInfo.reminder);
+        // get the reminder which are in future and populate the category
+        const reminders = await Reminder.find({ _id: { $in: userInfo.reminder }, date: { $gte: new Date() } }).populate('category');
+        res.json(reminders);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error.' });
@@ -18,8 +22,11 @@ router.get('/', authenticateToken, async (req, res) => {
 
 router.post('/create', authenticateToken, async (req, res) => {
     try {
+        const category = await Category.findOne({ name: req.body.category });
+        req.body.category = category._id;
+        const reminder = await Reminder.create(req.body);
         const userInfo = await UserInfo.findById({ _id: req.user._id });
-        userInfo.reminder.push({ id: req.body.title, number: req.body.date });
+        userInfo.reminder.push(reminder._id);
         await userInfo.save();
         res.status(201).json({ message: 'Reminder Created' });
     } catch (error) {
